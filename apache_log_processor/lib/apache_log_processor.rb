429602833be7e2f73cd090d91fd303d071bdf9a2
Mathew Chasan <aftermathew@gmail.com>
2009-05-04 00:32:31 -0700

    single threaded processing on apache log is working, missing only a few tests and options

--- apache_log_processor/lib/apache_log_processor.rb
+++ apache_log_processor/lib/apache_log_processor.rb
@@ -6,14 +6,20 @@ class ApacheLogProcessor
   VERSION = '1.0.0'
   CACHE_FILE_DEFAULT = './alp_cachepath'
   DEFAULT_NUM_THREADS = 20
-  @@ALP_CacheEntry = Struct.new(:name, :created_at)
+  DEFAULT_MAX_CACHE_AGE = (24 * 60 * 60) #24 hours in seconds
+  IP_PATTERN = /(\d+)\.(\d+)\.(\d+)\.(\d+)/
 
-  attr_accessor :num_threads, :cache
+  @@CacheEntry = Struct.new(:name, :created_at)
+
+  attr_accessor :num_threads, :cache, :max_cache_age
 
   def initialize logpath
+    @max_cache_age = DEFAULT_MAX_CACHE_AGE
     @cache = {}
     @logpath = logpath
     @num_threads = DEFAULT_NUM_THREADS
+    @log_data = []
+    @parsed_data = []
   end
 
   def load_cache cachepath=CACHE_FILE_DEFAULT
@@ -25,28 +31,66 @@ class ApacheLogProcessor
   end
 
   def get_ip line
-    line.match(/(\d+)\.(\d+)\.(\d+)\.(\d+)/)[0]
+    line.match(IP_PATTERN)[0]
   end
 
   def get_name_with_ip_using_network ip
-    Resolv.getname(ip)
+    begin
+      name = Resolv.getname(ip)
+      add_ip_and_name_to_cache(ip, name)
+      name
+    rescue
+      nil
+    end
+  end
+
+  def cache_record_should_be_removed record
+    ((Time.now - record[:created_at]) > @max_cache_age)
   end
 
   def get_name_with_ip_using_cache ip
-    @cache[ip][:name]
+    return nil unless cache[ip]
+
+    if(cache_record_should_be_removed cache[ip])
+      #not really needed, but it's nice to clear this stuff out at somepoint
+      cache[ip] = nil
+    else
+      cache[ip][:name]
+    end
   end
 
   def add_ip_and_name_to_cache ip, name
-    @cache[ip] = @@ALP_CacheEntry.new(name, Time.new)
+    @cache[ip] = @@CacheEntry.new(name, Time.new)
+  end
+
+  def parse_line line
+    ip = get_ip line
+
+    name = get_name_with_ip_using_cache(ip) ||
+      get_name_with_ip_using_network(ip) ||
+      ("Cannot find name for " + ip)
+
+    line.gsub(IP_PATTERN, name)
   end
 
   def read_logpath
+    @log_data = File.readlines(@logpath)
   end
 
-  def save_cache_to_disk
+  def save_cache_to_disk outfile
+    File.open(outfile, 'w') do |out|
+      YAML.dump(cache, out)
+    end
   end
 
-  def resolve_name ip
+  def run_line line_num
+    @parsed_data[line_num] = parse_line @log_data[line_num]
+  end
+
+  def run
+    read_logpath
+    @log_data.each_index{ |num| run_line(num) }
+    @parsed_data.each{ |index| p index }
   end
 
   def parse_options options
--- apache_log_processor/test/test_apache_log_processor.rb
+++ apache_log_processor/test/test_apache_log_processor.rb
@@ -4,9 +4,10 @@ require 'fileutils'
 
 class TestApacheLogProcessor < Test::Unit::TestCase
   def setup
-    @alp = ApacheLogProcessor.new 'sampleinput.log'
+    @alp = ApacheLogProcessor.new 'test/sampleinput.log'
     @ip = '208.77.188.166'
     @name = 'www.example.com'
+    @test_line = '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
   end
 
   def test_cache_load_raises_when_file_not_there
@@ -21,7 +22,7 @@ class TestApacheLogProcessor < Test::Unit::TestCase
   end
 
   def test_parse_line_returns_correct_ip
-    ip = @alp.get_ip '208.77.188.166 - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342'
+    ip = @alp.get_ip @test_line
     assert_equal(@ip, ip)
   end
 
@@ -38,6 +39,28 @@ class TestApacheLogProcessor < Test::Unit::TestCase
   def test_searching_for_an_ip_in_the_cache_works
     @alp.add_ip_and_name_to_cache(@ip, @name)
     assert_equal(@name, @alp.get_name_with_ip_using_cache(@ip))
+  end
+
+  def test_that_cache_retires_old_data
+    @alp.add_ip_and_name_to_cache(@ip, @name)
+    @alp.cache[@ip][:created_at] -= @alp.max_cache_age + 1
+    assert_nil(@alp.get_name_with_ip_using_cache(@ip))
+    assert_nil(@alp.cache[@ip])
+  end
+
+  def test_single_line_parse_replaces_ip_with_name
+    assert_equal("#{@name}" +
+                 ' - - [29/Apr/2009:16:07:38 -0700] "GET / HTTP/1.1" 200 1342',
+                 @alp.parse_line(@test_line))
+  end
+
+  def test_read_logpath_puts_file_data_into_log_data
+    @alp.read_logpath
+    assert(@alp.instance_eval("@log_data").instance_of?(Array))
+  end
+
+  def test_run
+    @alp.run
   end
 
   def teardown
